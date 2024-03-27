e291eff76bddea2926825122532eae092add9a41
RomitWorkSpace <romitkumar848@gmail.com>
2024-03-27 15:57:12 +0530

    All home page sections completed

--- resources/js/components/pages/Home.jsx
+++ resources/js/components/pages/Home.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import RaghuBanner from 'imgPath/sliders/RaghuBanner.jpeg'
+import AboutSection from '../HomeComponents/AboutSection'
+import HostelSection from '../HomeComponents/HostelSection'
+import FacilitySection from '../HomeComponents/FacilitySection'
+import GallerySection from '../HomeComponents/GallerySection'
+import TestimonialSection from '../HomeComponents/TestimonialSection'
+import NewsLetterSection from '../HomeComponents/NewsLetterSection'
 
 function Home() {
     return (
         <>
-        <h1>Welcome To Raghuma</h1>
+        <div className='container-fluid p-0'>
+            <img src={RaghuBanner} alt="Raghu" width="100%" />
+        </div>
+        <AboutSection />
+        <HostelSection />
+        <FacilitySection />
+        <GallerySection />
+        <TestimonialSection />
+        <NewsLetterSection />
         </>
     )
 }
