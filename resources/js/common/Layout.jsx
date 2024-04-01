b7f3adc7706055bd5e4d49370f0d0d65f8c08dfc
RomitWorkSpace <romitkumar848@gmail.com>
2024-04-01 17:00:18 +0530

    rest all pages added for routing

--- resources/js/common/Layout.jsx
+++ resources/js/common/Layout.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
 import Home from '../components/pages/Home';
+import About from '../components/pages/About';
 import Header from './Header';
 import Footer from './Footer';
+import Blog from '../components/pages/Blog';
+import Contact from '../components/pages/Contact';
+import HostelRoom from '../components/pages/HostelRoom';
+import RoomDetail from '../components/pages/RoomDetail';
+import Service from '../components/pages/Service';
 
 
 function Layout() {
@@ -11,7 +17,12 @@ function Layout() {
     <Header />
     <Routes>
         <Route path="/" exact element={<Home/>} />
-        {/* <Route path="/about" exact element={<About/>} /> */}
+        <Route path="/about" exact element={<About/>} />
+        <Route path="/blog" exact element={<Blog />} />
+        <Route path="/hostel" exact element={<HostelRoom />} />
+        <Route path="/room-detail" exact element={<RoomDetail />} />
+        <Route path="/service-detail" exact element={<Service />} />
+        <Route path="/contact" exact element={<Contact />} />
     </Routes>
     <Footer />
     </>
