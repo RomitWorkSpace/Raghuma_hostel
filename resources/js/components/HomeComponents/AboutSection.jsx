1fea3f0a16aa890053e6868404f2b76dbde8de75
RomitWorkSpace <romitkumar848@gmail.com>
2024-04-01 17:02:48 +0530

    Fade up animation added on scrolling page

--- resources/js/components/HomeComponents/AboutSection.jsx
+++ resources/js/components/HomeComponents/AboutSection.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import HomeAbout from 'imgPath/HomeAbout.png'
+import React, { useEffect } from 'react'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import HomeAbout from 'imgPath/circle-img.png'
+import { Link } from 'react-router-dom';
 
 function AboutSection() {
+    useEffect(() => {
+        AOS.init();
+      }, []);
     return (
         <>
-        <div className='container pt-5 mt-4'>
-            <div className='row'>
-                <div className='col-md-6'>
-                    <img src={ HomeAbout } alt="about" width="100%" style={{borderRadius:'50%'}} className='box-shadow' />
+        <div className='container pt-5 pb-5 mt-4' data-aos="fade-up">
+            <div className='row align-items-center'>
+                <div className='col-md-6 text-center'>
+                    <img src={ HomeAbout } alt="about" width="90%" style={{borderRadius:'50%'}} className='box-shadow' />
                 </div>
                 <div className='col-md-1'></div>
                 <div className='col-md-5'>
@@ -16,7 +22,9 @@ function AboutSection() {
                     <div className='text-justify'>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry</p>
-                        <button className='btn btn-raghuma'>Read More</button>
+                        <Link to="/about">
+                            <button className='btn btn-raghuma'>Read More</button>
+                        </Link>
                     </div>
                 </div>
             </div>
