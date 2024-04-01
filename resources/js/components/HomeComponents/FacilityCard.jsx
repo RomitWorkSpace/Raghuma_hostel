01f93c4103e63f45611e0e713afb69be903f44ee
RomitWorkSpace <romitkumar848@gmail.com>
2024-04-01 17:05:59 +0530

    icon width changed

--- resources/js/components/HomeComponents/FacilityCard.jsx
+++ resources/js/components/HomeComponents/FacilityCard.jsx
@@ -5,7 +5,7 @@ function FacilityCard({props}) {
         <>
         <div className='card-bg text-center'>
             <div className="icon-bg">
-            <img src={props.icon} width="90" alt="" />
+            <img src={props.icon} width="60" alt="" />
             </div>
             <h5 className='primary-clr bold mt-3'>{props.facility}</h5>
         </div>
