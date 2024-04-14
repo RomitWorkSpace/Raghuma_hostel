d6a2ec192afa1b708244e3191c2200c1e52c1da0
RomitWorkSpace <romitkumar848@gmail.com>
2024-04-14 10:45:37 +0530

    Hostel data fetched

--- resources/js/components/HomeComponents/HostelSection.jsx
+++ resources/js/components/HomeComponents/HostelSection.jsx
@@ -1,17 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import HostelCard from './HostelCard'
+import axios from 'axios';
 
 function HostelSection() {
+    const [hostels, setHostels] = useState([]);
+
+    useEffect(() => {
+        AOS.init();
+      }, []);
+
+      useEffect(() => {
+        axios.get(`/api/hostels`).then(res => {
+            if(res.status === 200)
+            {
+                setHostels(res.data.hostels)
+            }
+        })
+    },[]);
+  
     return (
         <>
-        <div className='container-fluid pt-5'>
+        <div className='container-fluid pt-5 pb-5 bg-pink' data-aos="fade-up">
             <h2 className='primary-clr lily-script text-center'>Our Hostels</h2>
             <h5 className='bold text-center'>M O R E &nbsp; &nbsp;A C C O M O D A T I O N S</h5>
             <div className='container pt-5'>
                 <div className='row'>
-                    <HostelCard />
-                    <HostelCard />
-                    <HostelCard />
+                {hostels.map((item) => (
+                    <HostelCard hostel = {item}/>
+                ))}
                 </div>
             </div>
         </div>
