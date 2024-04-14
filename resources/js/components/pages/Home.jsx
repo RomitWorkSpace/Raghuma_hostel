a61ccbe3b00e21e912a022a7f32bcbe3298258d3
RomitWorkSpace <romitkumar848@gmail.com>
2024-04-14 10:49:39 +0530

    Main slider added in home page

--- resources/js/components/pages/Home.jsx
+++ resources/js/components/pages/Home.jsx
@@ -1,18 +1,16 @@
 import React from 'react'
-import RaghuBanner from 'imgPath/sliders/RaghuBanner.jpeg'
 import AboutSection from '../HomeComponents/AboutSection'
 import HostelSection from '../HomeComponents/HostelSection'
 import FacilitySection from '../HomeComponents/FacilitySection'
 import GallerySection from '../HomeComponents/GallerySection'
 import TestimonialSection from '../HomeComponents/TestimonialSection'
 import NewsLetterSection from '../HomeComponents/NewsLetterSection'
+import MainSlider from '../HomeComponents/MainSlider'
 
 function Home() {
     return (
         <>
-        <div className='container-fluid p-0'>
-            <img src={RaghuBanner} alt="Raghu" width="100%" />
-        </div>
+        <MainSlider />
         <AboutSection />
         <HostelSection />
         <FacilitySection />
