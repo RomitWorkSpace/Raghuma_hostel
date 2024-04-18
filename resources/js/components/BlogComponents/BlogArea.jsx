0381951278e01aa6155ca8e7a0b980e302f705b6
RomitWorkSpace <romitkumar848@gmail.com>
2024-04-18 05:54:51 +0530

    Link added

--- resources/js/components/BlogComponents/BlogArea.jsx
+++ resources/js/components/BlogComponents/BlogArea.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import BlogImg from 'imgPath/blog.png'
 import axios from 'axios'
+import { Link } from 'react-router-dom';
 
 function BlogArea() {
     const [blogs, setBlogs] = useState([]);
@@ -21,10 +22,12 @@ function BlogArea() {
             <>
                 <div className='col-md-3 mb-4'>
                         <div className='blog-wrapper'>
+                            <Link to={`/blogs/${item.slug}`}>
                             <img src={`https://brandaile.com/public/images/blogs/${item.image}`} width="100%" alt="blog" style={{borderRadius: '8px 8px 0px 0px'}} className='rotate-in-2-fwd-cw' />
                             <div className='title-area text-center p-3'>
                                 <h5>{item.title}</h5>
                             </div>
+                            </Link>
                         </div>
                 </div>
             </>
