16048323f559f7d81353823420d2bd92a94fa3c6
RomitWorkSpace <romitkumar848@gmail.com>
2024-04-23 16:57:51 +0530

    Content updated

--- resources/js/components/HomeComponents/TestimonialSection.jsx
+++ resources/js/components/HomeComponents/TestimonialSection.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Profile from 'imgPath/profile.png'
+import Profile from 'imgPath/testimonial.png'
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
@@ -34,8 +34,9 @@ function TestimonialSection() {
                         </div>
                         <div className='col-md-1'></div>
                         <div className='col-md-4 text-justify'>
-                            <h5 className='primary-clr'>Marium Jessi</h5>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                            <h5 className='primary-clr'>Mohan Sharma</h5>
+                            <p>The facilities here are amazing. The rooms are spacious and comfortable, providing the perfect environment for both studying and relaxing. Plus, having access to amenities like the gym and laundry services right on site has made my life so much easier.</p>
+                            <p>Living at Raghuma Hostel has truly been a game-changer for me as a student. From the moment I stepped through the doors, I felt welcomed and embraced by the vibrant community that thrives here.</p>
                         </div>
                         <div className='col-md-2'></div>
                     </div>
@@ -50,8 +51,9 @@ function TestimonialSection() {
                         </div>
                         <div className='col-md-1'></div>
                         <div className='col-md-4 text-justify'>
-                            <h5 className='primary-clr'>Marium Jessi</h5>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                            <h5 className='primary-clr'>Krishna Taigor</h5>
+                            <p>Best hostel in greater Noida, in food quality my friend are their once I have tested food that was very testy even I suggest to my collogues once must visit Raghuma Boys Hostel when any one search hostel in greater Noida</p>
+                            <p>Choosing Raghuma Hostel has truly been one of the best decisions of my student life. From the moment I moved in, I felt like I was part of a big, welcoming family.</p>
                         </div>
                         <div className='col-md-2'></div>
                     </div>
