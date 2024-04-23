c679f5357b2f38b9086b4cb721b402c8fe1eefe4
RomitWorkSpace <romitkumar848@gmail.com>
2024-04-23 16:53:17 +0530

    Banner updated

--- resources/js/components/BlogComponents/BlogHero.jsx
+++ resources/js/components/BlogComponents/BlogHero.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import BlogImage from 'imgPath/blog.png'
+import BlogImage from 'imgPath/blogHero.jpeg'
 
 function BlogHero() {
     return (
@@ -17,9 +17,9 @@ function BlogHero() {
                     </div>
                     <div className='col-md-1'></div>
                     <div className='col-md-5 text-justify'>
-                        <h1 className='primary-clr lily-script'>Lorem Ipsum</h1>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printertook a galley of type andscrambled it to make a type specimen book.</p>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printertook a galley of type andscrambled it to make a type specimen book.</p>
+                        <h1 className='primary-clr lily-script'>Raghuma Hostel</h1>
+                        <p>With spacious rooms, modern amenities and a focus on cleanliness, we aim to set a new standard for student accommodation in India.</p>
+                        <p>Are you tired of compromising on comfort and hygiene? Are you looking for affordable accommodation that doesn't compromise on quality? Look no further than Raghuma Hostels. Join us and experience the Raghuma Hostels difference—where comfort, hygiene, and affordability converge to create the ultimate student living experience. Welcome home.</p>
                     </div>
                 </div>
             </div>
