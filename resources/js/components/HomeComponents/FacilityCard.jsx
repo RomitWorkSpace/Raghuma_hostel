d43c9ddf07a222455812f51079a517f4e80a54c9
RomitWorkSpace <romitkumar848@gmail.com>
2024-04-23 16:55:00 +0530

    page link applied on facility card

--- resources/js/components/HomeComponents/FacilityCard.jsx
+++ resources/js/components/HomeComponents/FacilityCard.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function FacilityCard({props}) {
     return (
         <>
-        <div className='card-bg text-center'>
+        <div className='card-bg text-center heartbeat'>
+            <Link to={props.link}>
             <div className="icon-bg">
             <img src={props.icon} width="60" alt="" />
             </div>
             <h5 className='primary-clr bold mt-3'>{props.facility}</h5>
+            </Link>
         </div>
         </>
     )
