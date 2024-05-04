e532cd163da8c151e6704325e256886001e415ab
RomitWorkSpace <romitkumar848@gmail.com>
2024-05-04 10:46:18 +0530

    Room detail page completed

---
+++ resources/js/components/pages/RoomDetail.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import DetailSection from '../RoomDetailComponents/DetailSection'
+
+function RoomDetail() {
+    return (
+        <>
+        <div className="container-fluid p-0">
+            <img src="" alt="banner" width="100%" />
+        </div>
+        <DetailSection />
+        </>
+    )
+}
+
+export default RoomDetail
