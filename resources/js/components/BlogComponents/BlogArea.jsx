3d89804723d5ff3eddd4d23eb893b1de1f0a2107
RomitWorkSpace <romitkumar848@gmail.com>
2024-05-09 18:23:52 +0530

    image path updated

--- resources/js/components/BlogComponents/BlogArea.jsx
+++ resources/js/components/BlogComponents/BlogArea.jsx
@@ -23,7 +23,7 @@ function BlogArea() {
                 <div className='col-md-3 mb-4'>
                         <div className='blog-wrapper'>
                             <Link to={`/blogs/${item.slug}`}>
-                            <img src={`https://brandaile.com/public/images/blogs/${item.image}`} width="100%" alt="blog" style={{borderRadius: '8px 8px 0px 0px'}} className='rotate-in-2-fwd-cw' />
+                            <img src={`/public/images/blogs/${item.image}`} width="100%" alt="blog" style={{borderRadius: '8px 8px 0px 0px'}} className='rotate-in-2-fwd-cw' />
                             <div className='title-area text-center p-3'>
                                 <h5>{item.title}</h5>
                             </div>
