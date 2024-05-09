1cf155e1ba2be57dc2965504aa41be87a00e3d9d
RomitWorkSpace <romitkumar848@gmail.com>
2024-05-09 18:24:47 +0530

    image path updated

--- resources/js/components/HomeComponents/HostelCard.jsx
+++ resources/js/components/HomeComponents/HostelCard.jsx
@@ -6,11 +6,11 @@ function HostelCard({hostel}) {
         <>
         <div className='col-md-4 mb-4'>
             <div className='card-shadow p-3 bg-white'>
-            <img src={`https://raghumahostels.com/public/images/rooms/${hostel.image}`} width="100%" alt="hostel" />
+            <img src={`/public/images/rooms/${hostel.image}`} width="100%" alt="hostel" />
             <div className='content text-center pt-3'>
                 <h4 className='primary-clr'>{ hostel.name }</h4>
                 <div className='text-center mt-4'>
-                    <Link to={`https://raghumahostels.com/hostels/${hostel.slug}`}>
+                    <Link to={`/hostels/${hostel.slug}`}>
                     <button className='btn btn-raghuma'>View Rooms</button>
                     </Link>
                 </div>
