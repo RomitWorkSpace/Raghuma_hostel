9e60c25f1f05496a6bf64086f492eb212cf3b804
RomitWorkSpace <romitkumar848@gmail.com>
2024-07-27 20:40:41 +0530

    Helmet component imported for seo meta data

--- resources/js/components/pages/Blog.jsx
+++ resources/js/components/pages/Blog.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Helmet } from 'react-helmet';
 import BlogBanner from 'imgPath/banners/blog-banner.jpeg'
 import BlogHero from '../BlogComponents/BlogHero'
 import BlogArea from '../BlogComponents/BlogArea'
@@ -6,6 +7,11 @@ import BlogArea from '../BlogComponents/BlogArea'
 function Blog() {
     return (
         <>
+        <Helmet>
+            <title>Blogs - Raghuma Hostel | top hostel in greater noida</title>
+            <meta name="keywords" content="top hostel in greater noida,hostel in knowlege park 3,best hostel near me,best 3 seater hostel near me,Online hostel in knowlege park 3" />
+            <meta name="description" content="Explore insightful content and stay updated with Raghuma Hostel's blogs." />
+        </Helmet>
         <div className='container-fluid p-0'>
             <img src={ BlogBanner } width="100%" alt="blog" />
         </div>
